04414e41b7f6ceaa16e128a6b3eda8f0c6a8e9f3
duyuqian <809009017@qq.com>
2020-08-08 16:19:47 +0800

    [duyuqian] fix: create the sequence only with start and end

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/Sequence.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/Sequence.java
@@ -1,7 +1,5 @@
 package com.cultivation.javaBasic.showYourIntelligence;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.Iterator;
 
 public class Sequence implements Iterable<Integer> {
@@ -9,7 +7,9 @@ public class Sequence implements Iterable<Integer> {
     private final Integer end;
 
     public Sequence(Integer start, Integer end) {
-        if (start >= end) { throw new IllegalArgumentException("Start must be smaller than End."); }
+        if (start >= end) {
+            throw new IllegalArgumentException("Start must be smaller than End.");
+        }
         this.start = start;
         this.end = end;
     }
@@ -23,13 +23,17 @@ public class Sequence implements Iterable<Integer> {
 class SequenceIterator implements Iterator<Integer> {
     // TODO: You can add additional fields or methods if you want.
     // <--start
-
+    Integer start;
+    Integer end;
+    Integer now;
     // --end-->
 
     SequenceIterator(Integer start, Integer end) {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        this.start = start;
+        this.end = end;
+        this.now = start;
         // --end-->
     }
 
@@ -37,7 +41,7 @@ class SequenceIterator implements Iterator<Integer> {
     public boolean hasNext() {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        return now < end;
         // --end-->
     }
 
@@ -45,7 +49,7 @@ class SequenceIterator implements Iterator<Integer> {
     public Integer next() {
         // TODO: please implements the following code to pass the test
         // <--start
-        throw new NotImplementedException();
+        return now++;
         // --end-->
     }
 }
