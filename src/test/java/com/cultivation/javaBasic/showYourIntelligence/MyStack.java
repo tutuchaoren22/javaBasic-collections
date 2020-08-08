ba930cda7ab55a5cf3fe79a94f17a7dd21cfcd00
duyuqian <809009017@qq.com>
2020-08-08 14:14:51 +0800

    [duyuqian] fix: finish myStack to pass ArrayTest

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/MyStack.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/MyStack.java
@@ -1,7 +1,5 @@
 package com.cultivation.javaBasic.showYourIntelligence;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
 public class MyStack {
     private int[] storage;
@@ -26,7 +24,8 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-        throw new NotImplementedException();
+        storage[count] = value;
+        count++;
         // --end-->
     }
 
@@ -36,7 +35,12 @@ public class MyStack {
         // TODO: Please create a new array of size newCapacity. And update related fields
         // TODO: You SHOULD NOT USE COLLECTIONS OTHER THAN ARRAY.
         // <--start
-        throw new NotImplementedException();
+        capacity = newCapacity;
+        int[] newStorage = new int[capacity];
+        for (int i = 0; i < storage.length; i++) {
+            newStorage[i] = storage[i];
+        }
+        storage = newStorage;
         // --end-->
     }
 
@@ -54,8 +58,7 @@ public class MyStack {
     private int pop() {
         // TODO: Please pop one element from the array.
         // <--start
+        return storage[--count];
         // --end-->
-
-        throw new UnsupportedOperationException("Stack is empty.");
     }
 }
